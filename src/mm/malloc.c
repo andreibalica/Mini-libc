fc2d1114384dde65917b235ba6c928c3dcb0921c
andrei.balica <andrei.balica@stud.acs.upb.ro>
2024-10-27 14:54:59 +0200

    third commit

--- src/mm/malloc.c
+++ src/mm/malloc.c
@@ -1,48 +1,54 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
+#include <internal/essentials.h>
 #include <internal/mm/mem_list.h>
 #include <internal/types.h>
-#include <internal/essentials.h>
-#include <sys/mman.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
 
-void *malloc(size_t size)
-{
-	void *ret = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	if(ret == MAP_FAILED)
-		return NULL;
-	mem_list_head.len = size;
-	mem_list_add(ret, size);
-	return ret;
+void *malloc(size_t size) {
+  void *ret = mmap(NULL, size, PROT_READ | PROT_WRITE,
+                   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (ret == MAP_FAILED)
+    return NULL;
+  mem_list_add(ret, size);
+  return ret;
 }
 
-void *calloc(size_t nmemb, size_t size)
-{
-	size_t total_size = nmemb * size;
-	void *ret = mmap(NULL, total_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	if(ret == MAP_FAILED) {
-		return NULL;
-	}
-	memset(ret, 0, total_size);
-    mem_list_head.len = total_size;
-    mem_list_add(ret, total_size);
-	return ret;
+void *calloc(size_t nmemb, size_t size) {
+  size_t total_size = nmemb * size;
+  void *ret = mmap(NULL, total_size, PROT_READ | PROT_WRITE,
+                   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (ret == MAP_FAILED) {
+    return NULL;
+  }
+  memset(ret, 0, total_size);
+  mem_list_add(ret, total_size);
+  return ret;
 }
 
-void free(void *ptr)
-{
-	/* TODO: Implement free(). */
+void free(void *ptr) { /* TODO: Implement free(). */ }
+
+void *realloc(void *ptr, size_t size) {
+  void *ret = mmap(NULL, size, PROT_READ | PROT_WRITE,
+                   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (ret == MAP_FAILED) {
+    return NULL;
+  }
+  void *new = malloc(size);
+  memcpy(new, ret, size);
+  return new;
 }
 
-void *realloc(void *ptr, size_t size)
-{
-	/* TODO: Implement realloc(). */
-	return NULL;
-}
-
-void *reallocarray(void *ptr, size_t nmemb, size_t size)
-{
-	/* TODO: Implement reallocarray(). */
-	return NULL;
+void *reallocarray(void *ptr, size_t nmemb, size_t size) {
+  size_t total_size = nmemb * size;
+  void *ret = mmap(NULL, total_size, PROT_READ | PROT_WRITE,
+                   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (ret == MAP_FAILED) {
+    return NULL;
+  }
+  void *new = malloc(total_size);
+  memcpy(new, ret, total_size);
+  return new;
 }
